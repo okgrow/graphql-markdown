3953a45d814f3ff54faa994b2be0262b7446e820
Charles Nelson <charlesnelson28@gmail.com>
2017-08-11 15:00:53 -0400

    chore: prettier installed & setup. format script added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": "airbnb",
-  "env": {
+  parser: 'babel-eslint',
+  extends: ['prettier', 'airbnb'],
+  env: {
     jest: true,
     node: true,
     es6: true,
   },
-  "rules": {
-    "max-len": 1, // Warn as sometimes necessary to have long strings to not risk whitespace
-    "new-cap": 1, // Warn only as we don't have any control over external packages doing this
-    "import/named": 2, // Ensure named imports correspond to a named export in the remote file
-    "no-mixed-operators": 0, // Allow && || usage. e.g: const foo = a && a.foo || undefined;
-    "arrow-parens": ["error", "as-needed"], // Allow usage of: foo => {...}
-  }
-}
+  rules: {
+    'max-len': 1, // Warn as sometimes necessary to have long strings to not risk whitespace
+    'new-cap': 1, // Warn only as we don't have any control over external packages doing this
+    'import/named': 2, // Ensure named imports correspond to a named export in the remote file
+    'no-mixed-operators': 0, // Allow && || usage. e.g: const foo = a && a.foo || undefined;
+    'arrow-parens': ['error', 'as-needed'], // Allow usage of: foo => {...}
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 80,
+      },
+    ],
+  },
+  plugins: ['graphql', 'prettier'],
+};
