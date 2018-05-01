2448d5598855940142c079e855cd4eb6119e5ea9
Charles Nelson <charlesnelson28@gmail.com>
2018-04-30 22:06:48 -0400

    chore: replace package-lock.json for jest --coverage to run

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -9206,136 +9206,6 @@ var stripBomString = function(str) {
   return str;
 };
 
-var toString$2 = Object.prototype.toString;
-
-var kindOf$1 = function kindOf(val) {
-  if (val === void 0) return 'undefined';
-  if (val === null) return 'null';
-
-  var type = typeof val;
-  if (type === 'boolean') return 'boolean';
-  if (type === 'string') return 'string';
-  if (type === 'number') return 'number';
-  if (type === 'symbol') return 'symbol';
-  if (type === 'function') {
-    return isGeneratorFn$1(val) ? 'generatorfunction' : 'function';
-  }
-
-  if (isArray$2(val)) return 'array';
-  if (isBuffer$2(val)) return 'buffer';
-  if (isArguments$1(val)) return 'arguments';
-  if (isDate$1(val)) return 'date';
-  if (isError$1(val)) return 'error';
-  if (isRegexp$1(val)) return 'regexp';
-
-  switch (ctorName$1(val)) {
-    case 'Symbol': return 'symbol';
-    case 'Promise': return 'promise';
-
-    // Set, Map, WeakSet, WeakMap
-    case 'WeakMap': return 'weakmap';
-    case 'WeakSet': return 'weakset';
-    case 'Map': return 'map';
-    case 'Set': return 'set';
-
-    // 8-bit typed arrays
-    case 'Int8Array': return 'int8array';
-    case 'Uint8Array': return 'uint8array';
-    case 'Uint8ClampedArray': return 'uint8clampedarray';
-
-    // 16-bit typed arrays
-    case 'Int16Array': return 'int16array';
-    case 'Uint16Array': return 'uint16array';
-
-    // 32-bit typed arrays
-    case 'Int32Array': return 'int32array';
-    case 'Uint32Array': return 'uint32array';
-    case 'Float32Array': return 'float32array';
-    case 'Float64Array': return 'float64array';
-  }
-
-  if (isGeneratorObj$1(val)) {
-    return 'generator';
-  }
-
-  // Non-plain objects
-  type = toString$2.call(val);
-  switch (type) {
-    case '[object Object]': return 'object';
-    // iterators
-    case '[object Map Iterator]': return 'mapiterator';
-    case '[object Set Iterator]': return 'setiterator';
-    case '[object String Iterator]': return 'stringiterator';
-    case '[object Array Iterator]': return 'arrayiterator';
-  }
-
-  // other
-  return type.slice(8, -1).toLowerCase().replace(/\s/g, '');
-};
-
-function ctorName$1(val) {
-  return val.constructor ? val.constructor.name : null;
-}
-
-function isArray$2(val) {
-  if (Array.isArray) return Array.isArray(val);
-  return val instanceof Array;
-}
-
-function isError$1(val) {
-  return val instanceof Error || (typeof val.message === 'string' && val.constructor && typeof val.constructor.stackTraceLimit === 'number');
-}
-
-function isDate$1(val) {
-  if (val instanceof Date) return true;
-  return typeof val.toDateString === 'function'
-    && typeof val.getDate === 'function'
-    && typeof val.setDate === 'function';
-}
-
-function isRegexp$1(val) {
-  if (val instanceof RegExp) return true;
-  return typeof val.flags === 'string'
-    && typeof val.ignoreCase === 'boolean'
-    && typeof val.multiline === 'boolean'
-    && typeof val.global === 'boolean';
-}
-
-function isGeneratorFn$1(name, val) {
-  return ctorName$1(name) === 'GeneratorFunction';
-}
-
-function isGeneratorObj$1(val) {
-  return typeof val.throw === 'function'
-    && typeof val.return === 'function'
-    && typeof val.next === 'function';
-}
-
-function isArguments$1(val) {
-  try {
-    if (typeof val.length === 'number' && typeof val.callee === 'function') {
-      return true;
-    }
-  } catch (err) {
-    if (err.message.indexOf('callee') !== -1) {
-      return true;
-    }
-  }
-  return false;
-}
-
-/**
- * If you need to support Safari 5-7 (8-10 yr-old browser),
- * take a look at https://github.com/feross/is-buffer
- */
-
-function isBuffer$2(val) {
-  if (val.constructor && typeof val.constructor.isBuffer === 'function') {
-    return val.constructor.isBuffer(val);
-  }
-  return false;
-}
-
 var utils = createCommonjsModule(function (module, exports) {
 
 
@@ -9354,13 +9224,13 @@ exports.define = function(obj, key, val) {
  * Returns true if `val` is a buffer
  */
 
-exports.isBuffer = val => kindOf$1(val) === 'buffer';
+exports.isBuffer = val => kindOf(val) === 'buffer';
 
 /**
  * Returns true if `val` is an object
  */
 
-exports.isObject = val => kindOf$1(val) === 'object';
+exports.isObject = val => kindOf(val) === 'object';
 
 /**
  * Cast `input` to a buffer
@@ -9451,7 +9321,7 @@ function aliase(name) {
 
 var stringify = function(file, data, options) {
   if (data == null && options == null) {
-    switch (kindOf$1(file)) {
+    switch (kindOf(file)) {
       case 'object':
         data = file.data;
         options = {};
@@ -9535,11 +9405,11 @@ var excerpt = function(file, options) {
  */
 
 var toFile = function(file) {
-  if (kindOf$1(file) !== 'object') {
+  if (kindOf(file) !== 'object') {
     file = { content: file };
   }
 
-  if (kindOf$1(file.data) !== 'object') {
+  if (kindOf(file.data) !== 'object') {
     file.data = {};
   }
 
